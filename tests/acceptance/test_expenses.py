d37dbfe9046892af5274ac00a1280221f40828a6
Antoni P. Oleksicki <ap.oleksicki@gmail.com>
2021-01-17 16:10:52 +0100

    Add acceptance test for adding expense

--- tests/acceptance/test_expenses.py
+++ tests/acceptance/test_expenses.py
@@ -1,5 +1,13 @@
+import subprocess
+from unittest import expectedFailure
 from unittest import TestCase
 
 
 class TestExpenses(TestCase):
-    pass
+    @expectedFailure
+    def test_user_can_add_expense(self) -> None:
+        self.user_adds_expense()
+
+    def user_adds_expense(self) -> None:
+        result = subprocess.run(['budgxeto', 'add-expense', '100'])
+        self.assertEqual(0, result)
