2a69422b04358a7b034dbebdbe63eafad470e643
Antoni P. Oleksicki <ap.oleksicki@gmail.com>
2021-01-17 18:10:02 +0100

    Add dummy add expense command handler

--- budgxeto/cli.py
+++ budgxeto/cli.py
@@ -1,12 +1,11 @@
 import sys
 from typing import Callable
 
+from budgxeto.generic import Command
 from budgxeto.generic import CommandHandler
 
 
-def create_add_expense(
-        handler: CommandHandler = None,
-) -> Callable[[str], None]:
+def create_add_expense(handler: CommandHandler) -> Callable[[str], None]:
     def add_expense(amount: str) -> None:
         pass
     return add_expense
@@ -18,7 +17,11 @@ def list_expenses() -> None:
 
 
 def main() -> None:
-    add_expense = create_add_expense()
+    class AddExpenseCommandHandler(CommandHandler):
+        def handle(self, command: Command) -> None:
+            pass
+
+    add_expense = create_add_expense(AddExpenseCommandHandler())
 
     if sys.argv[1] == 'add-expense':
         add_expense('50')
